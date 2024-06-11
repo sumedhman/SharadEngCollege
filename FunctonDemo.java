f52ae913addcc0b293df577308bfe17e0888b648
sumedh <37269510+sumedhman@users.noreply.github.com>
2024-06-11 14:59:15 +0530

    This file is releted to function

---
+++ FunctonDemo.java
@@ -0,0 +1,14 @@
+package sam.com;
+
+public class FunctonDemo {
+	public static void add() {
+		int a=  10;
+		int b=20;
+		int total= a+b;
+		System.out.println("addition :"+total);
+	}
+	public static void sub(int a,int b) {
+		int total= a-b;
+		System.out.println("sub :"+total);
+	}
+}
---
+++ TestFunctions.java
@@ -0,0 +1,13 @@
+package sam.com;
+
+public class TestFunctions {
+
+	public static void main(String[] args) {
+		//System.out.println("hello");
+		//FunctonDemo fd=new FunctonDemo();  //object creation
+		FunctonDemo.add();
+		FunctonDemo.sub(12, 15);
+		//go to github.com
+	}
+
+}
